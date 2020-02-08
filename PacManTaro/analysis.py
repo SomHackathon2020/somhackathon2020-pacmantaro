fc76aca24b5ba3f72947023f90ea7471fa4e581a
Eric Alcaide <ericalcaide@MacBook-Pro-de-Eric.local>
2020-02-08 21:35:09 +0100

    heyheyhey

--- PacManTaro/analysis.py
+++ PacManTaro/analysis.py
@@ -9,6 +9,22 @@ from __init__ import db
 
 analysis = Blueprint('analysis', __name__)
 
-@analysis.route('/testanalysis')
-def testanalysis():
-    return "<b>Works!</b>"
+@analysis.route('/user_acts/<user_id>', methods=['GET'])
+def user_acts(user_id):
+	acts = Activitat.query.filter_by(user_id=user_id)
+	acts = acts if acts else []
+	return render_template("act_list.html", acts = acts)
+
+
+@analysis.route('/act/<ide>', methods=['GET'])
+def act(ide):
+	acts = Activitat.query.filter_by(id=ide)
+	acts = acts if acts else []
+	return render_template("act_list.html", acts = acts)
+
+
+@analysis.route('/test/<ide>', methods=['GET'])
+def testing(ide):
+	acts = Activitat.query.filter_by(id=ide)
+	acts = acts if acts else []
+	return render_template("act_list.html", acts = acts)
